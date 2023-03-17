430321b28c95f7fcfdda0a08b533dd50e52deb53
James Miller <jam1211miller@gmail.com>
2023-03-16 22:05:03 -0700

    updated list

--- src/components/links.js
+++ src/components/links.js
@@ -1,5 +1,6 @@
 const linkList = [
   '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.3527435045044!2d-112.0336423845485!3d33.46616588076946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b0d0d232b3331%3A0xa0f720e16694d1e2!2sDeseo%20A%20Coffee%20Shack!5e0!3m2!1sen!2sus!4v1679028423933!5m2!1sen!2sus" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+  '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21810.372405647064!2d-112.03319299001282!3d33.48812037870933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b0d9bc5d08e19%3A0x12d26cb99edc2f3e!2sGlai%20Baan!5e0!3m2!1sen!2sus!4v1679029156947!5m2!1sen!2sus" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
 ];
 
 export default linkList;
