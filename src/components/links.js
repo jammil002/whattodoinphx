cdbbab08cc86bd7417618022a397e3b8dcccbe4e
James Miller <jam1211miller@gmail.com>
2023-03-16 21:56:18 -0700

    Updated location and links. Frames will now  used

--- src/components/links.js
+++ src/components/links.js
@@ -1,5 +1,5 @@
 const linkList = [
-  "https://duckduckgo.com/?t=ffab&q=deseo+coffee+shack+phoenix&atb=v345-1&ia=web&iaxm=places",
+  '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.3527435045044!2d-112.0336423845485!3d33.46616588076946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b0d0d232b3331%3A0xa0f720e16694d1e2!2sDeseo%20A%20Coffee%20Shack!5e0!3m2!1sen!2sus!4v1679028423933!5m2!1sen!2sus" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
 ];
 
 export default linkList;
